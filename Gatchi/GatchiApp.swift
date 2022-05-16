f047a2dce25ebcb8467a073055ff499fae10af58
Angela Zhang <angelazhang331@ucla.edu>
2022-05-16 10:55:48 -0700

    initial

---
+++ Gatchi/GatchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GatchiApp.swift
+//  Gatchi
+//
+//  Created by Angela Zhang on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct GatchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
